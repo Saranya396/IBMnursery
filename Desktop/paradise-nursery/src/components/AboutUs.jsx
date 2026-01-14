5e63177815f2bbb8038ba8ca37a54642dd4abef5
tirumalaSaranya396 <perumallatirumalasaranya1@gmail.com>
2026-01-14 10:59:15 +0530

    Added AboutUs component

---
+++ Desktop/paradise-nursery/src/components/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is a one-stop destination for high-quality indoor and
+        outdoor plants. We are passionate about bringing nature closer to your
+        home and workspace.
+      </p>
+
+      <p>
+        Our mission is to provide healthy, affordable plants while promoting a
+        greener and more sustainable lifestyle. From decorative houseplants to
+        garden essentials, we carefully curate every plant we sell.
+      </p>
+
+      <p>
+        At Paradise Nursery, customer satisfaction and plant care are our top
+        priorities. Thank you for choosing us to be a part of your green
+        journey!
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
